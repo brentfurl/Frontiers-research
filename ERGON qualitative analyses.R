c9e9ff16b83c2f26bbf8ea314ee4944406858b8e
brentfurl <brentfurl@gmail.com>
2023-02-20 14:21:33 -0600

    Initial commit

---
+++ ERGON qualitative analyses.R
@@ -0,0 +1,41 @@
+##### ERGON qualitative analyses
+
+df_labelled_gsheet <- "https://docs.google.com/spreadsheets/d/1vQgXI6KzRiaT4nHrOZlh8Fw27hDqidvl2bw2fq6P7Ag/edit#gid=1415113395"
+df_change_gsheet <- "https://docs.google.com/spreadsheets/d/138WUGnw9qT8ebrjkymcoKbwYQJFBr5xJgvuOC7jEg2I/edit#gid=280769640"
+df_change <- read_sheet(df_change_gsheet)
+
+
+df_labelled_read <- read_sheet(df_labelled_gsheet, sheet = "Final") %>% 
+  rename(org_valence = "org valence (1-5) 5 = worst", amount_change = "degree(amount) of change (1-5) 5 = most") %>% 
+  filter(!is.na(org_valence))
+
+df_labelled <- df_labelled_read %>% 
+  inner_join(., df_change) %>% # df_change is the other dataset in gdrive called
+  unite("tags", tag1:tag16, sep = ",", na.rm = TRUE) %>% 
+  mutate(valence_amount_sum = org_valence + amount_change) %>% 
+  select(ResponseId, company, Year, org_valence, amount_change, valence_amount_sum, tags, TENURE:COMPANY)
+
+measures <- c("org_valence", "amount_change", "valence_amount_sum")
+measures_loop <- list()
+
+for(m in 1:length(measures)){
+  measures_loop[[m]] <- tidy(t.test(get(measures[[m]]) ~ Year, data = df_labelled)) %>% 
+  select(p.value) %>% 
+  add_column(measure = measures[[m]]) %>% 
+  add_column(mean2019 = mean(df_labelled %>% filter(Year == "2019") %>% pull(get(measures[[m]]))),
+             mean2022 = mean(df_labelled %>% filter(Year == "2022") %>% pull(get(measures[[m]])))) %>% 
+  add_column(`22minus19-meanDiff` = mean(df_labelled %>% filter(Year == "2022") %>% pull(get(measures[[m]])))- 
+               mean(df_labelled %>% filter(Year == "2019") %>% pull(get(measures[[m]]))),.before=1) %>% 
+    add_column(n19n22 = str_c(df_labelled %>% group_by(Year) %>% count() %>% arrange(Year) %>% pull(n), collapse = ", ")) %>% 
+  mutate(meaning = case_when(
+    sign(`22minus19-meanDiff`) == -1 & p.value <= .05 ~ "2019 shows sig higher negativity",
+    sign(`22minus19-meanDiff`) == -1 & p.value <= .1 ~ "2019 shows marginally higher negativity",
+    sign(`22minus19-meanDiff`) == -1 & p.value <= .2 ~ "2019 is trending toward higher negativity",
+    sign(`22minus19-meanDiff`) == -1 & p.value > .2 ~ "2019 is higher but not noteworthy yet",
+    sign(`22minus19-meanDiff`) == 1 & p.value <= .05 ~ "2022 shows sig higher negativity",
+    sign(`22minus19-meanDiff`) == 1 & p.value <= .1 ~ "2022 shows marginally higher negativity",
+    sign(`22minus19-meanDiff`) == 1 & p.value <= .2 ~ "2022 is trending toward higher negativity",
+    sign(`22minus19-meanDiff`) == 1 & p.value > .2 ~ "2022 is higher in negativity but not noteworthy yet"))
+}
+    
+t_tests <- bind_rows(measures_loop) %>% select(measure, p.value, meaning, everything())
---
+++ get data.R
@@ -0,0 +1,38 @@
+# get data and dates for ergon, rt and mcc
+pacman::p_load("tidyverse")
+
+# MCC - see MCC.2 "DATA PREP_2021plus2019 # The CPIs are different.  Also, 2019 does not measure positivity and satisfaction
+  
+  
+### ERGON  # don't need to run the code below again
+
+ergon_sept2019 <- readRDS("~/Dropbox/Clients/Frontiers research/Ergon_2019.rds") %>%
+  mutate(Year = replace_na("2019")) %>%
+  add_column(month = "September")# see (DCI/)Clients/Ergon/data prep Ergon 2019.R
+ergon_oct2022 <- readRDS("~/Dropbox/Clients/Frontiers research/ergon_oct2022.rds") %>% rename(LOCATION = "REGION") %>%
+  add_column(month = "September") # reran the data prep script carefully and added df_change to the set
+ colnames(ergon_sept2019)[1:68] <- colnames(ergon_oct2022)[1:68] # there were some difference in company vs organization in the item language
+ergon <- full_join(ergon_sept2019, ergon_oct2022) %>% add_column(company = "Ergon")
+saveRDS(ergon, "~/Dropbox/Clients/Frontiers research/ergon.rds")
+
+######
+
+MCC <- readRDS("~/Dropbox/Clients/Frontiers research/MCC.rds") %>% filter(!is.na(Change))
+ergon <- readRDS("~/Dropbox/Clients/Frontiers research/ergon.rds")
+
+df_change_erg <- ergon %>% 
+  select(ResponseId, Change, nps, company, Year, month, TENURE, GENERATION, ROLE, LOCATION, COMPANY)
+df_change_mcc <- MCC %>% 
+  select(ResponseId, Change, nps, company, Year, month, TENURE, GENERATION, ROLE, LOCATION, GENDER)
+
+df_change <- df_change_erg %>% 
+  full_join(., df_change_mcc) %>% 
+  select(ResponseId, nps:GENDER, Change) %>% 
+  filter(!is.na(Change) & Change != "N/A" & Change != "N/a" & Change != "n/a")
+
+write_csv(df_change, "~/Dropbox/Clients/Frontiers research/df_change_frontiers.csv")
+
+drive_trash("df_change_labelling")
+gs4_create(name = "df_change_labelling", sheets = list(
+  df = df_change %>% select(ResponseId, company, Change) %>% arrange(ResponseId)))#, anovas = anovas_sig_no_nests_basic, nps_regression = model))
+drive_mv("df_change_labelling", path = paste0("~/DCI/Frontiers pre-post research/data/"))   
